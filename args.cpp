296f1dcab93009a4d797aa5b0c158ab2604d92cb
Herman Singh Umrao <63940157+hermanumrao@users.noreply.github.com>
2022-10-10 20:57:57 +0530

    Add files via upload

---
+++ args.cpp
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std ;
+int main(int argc, char* argv[])
+{
+	int sum=0;
+	cout<<"number of arguments="<<argc<<endl;
+
+	for(int i=1; i<argc;i++)
+	{
+		cout<<i<<"-->"<<argv[i]<<endl;
+		sum+=atoi(argv[i]);
+	}
+	cout<<"sum="<<sum<<endl;
+	return 0;
+}
+
+
+/* -->OUTPUT<--
+arvind@arvind-ThinkBook-14-G2-ITL-Ub:~/Desktop/code$ g++ -g args.cpp
+arvind@arvind-ThinkBook-14-G2-ITL-Ub:~/Desktop/code$ ./a.out 1 2 3 4
+number of arguments=5
+1-->1
+2-->2
+3-->3
+4-->4
+sum=10
+arvind@arvind-ThinkBook-14-G2-ITL-Ub:~/Desktop/code$ ./a.out 1 2 3
+number of arguments=4
+1-->1
+2-->2
+3-->3
+sum=6
+arvind@arvind-ThinkBook-14-G2-ITL-Ub:~/Desktop/code$ ./a.out
+number of arguments=1
+sum=0
+*/
