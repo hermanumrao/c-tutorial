5e636ce9b82f2d7a751e8857d785239b2e73f25c
Herman Singh Umrao <63940157+hermanumrao@users.noreply.github.com>
2022-10-16 22:55:13 +0530

    Update suffix_trie.cpp

--- data_struct/trie/suffix_trie.cpp
+++ data_struct/trie/suffix_trie.cpp
@@ -11,21 +11,23 @@ using namespace std ;
 
 struct stnode
 {
-    bool leaf;
-    struct stnode *children[alpha_max];
-    char chr[str_len];
     int start;
     int end;
-    // int suffixIndex;
+    struct stnode *children[alpha_max];
 };
 
-stnode *init_node(int start1,int end1,bool leaf1)
+int pos(char a)
+{
+    if (a=='#') return 26;
+    else return (int(a)-97);
+}
+
+stnode *init_node(int start1,int end1)
 {
     stnode *node = new stnode();
     node->start=start1;
     node->end=end1;
     // node->suffixIndex=0;
-    node->leaf=leaf1;
     for (int i=0; i<alpha_max; i++)
     {
         node->children[i]=NULL;
@@ -37,6 +39,11 @@ stnode *init_node(int start1,int end1,bool leaf1)
 
 void stdisplay(stnode *root,const char *st)
 {
+    for (int itt=0;itt<alpha_max;itt++) if(root->children[itt]!=NULL) 
+    {
+        cout<<"#"<<typeid(root->children[itt]).name()<<root->children[itt]->start<<root->children[itt]->end;
+    }
+    cout<<endl;
     for (int it2=0;it2<alpha_max;it2++)
     {
         if(root->children[it2]!=NULL)
@@ -46,40 +53,83 @@ void stdisplay(stnode *root,const char *st)
                 cout<<st[it3];
             }
             cout<<":";
-            if (root->children[it2]->leaf==true) cout<<"^"<<endl;
-            else stdisplay(root->children[it2],st);
+            if (root->children[it2]->end==strlen(st)) cout<<"^"<<endl;
+            else 
+            {
+                
+                cout<<"#"<<typeid(root->children[it2]).name()<<root->children[it2]->start<<root->children[it2]->end;
+                stdisplay(root->children[it2],st);
+            }
 
         }
     }
 }
 
+int check_similar(int start,int start2,int end,const char *st)
+{
+    int x;
+    int tmp;
+    if ((start-start2)>(end-start))
+    {
+        tmp=end-start;
+    }
+    else tmp=start-start2;
+    for (int it4=0;it4<tmp;it4++)
+    {
+        if(st[start2+it4]==st[start+it4]) x=x+1;
+        
+    }
+    
+    return x;
+}
+
+stnode *overwrite(stnode *root,const char *st,int start2)
+{
+    int start=root->start;
+    int end=root->end;
+    int x =check_similar(start,start2,end,st);
+    if(start2<start)
+    {
+        root->end=(start+x);
+        if (x<=(end-start))
+        {
+            stnode *newNode1=init_node(start2+x+1,end);
+            stnode *newNode2=init_node(start+x+1,end);
+            root->children[pos(st[start2+x])]=newNode1;
+            root->children[pos(st[start+x])]=newNode2;
+            
+        }
+    }
+    stdisplay(root,st);
+    return root;
+}
+
 int main()
 {
-    char st[str_len] ="abcdaef";
+    char st[str_len] ="abcab";
     int tmp = strlen(st);
     st[tmp]='#';
     cout<<st<<tmp<<endl;
-    stnode *root=init_node(1,tmp,false);
-    for (int it=0; (it+1)<strlen(st);it++)
+    stnode *root=init_node(1,tmp);
+    for (int it=strlen(st)-1; (it+1)>0;it--)
     {
-        if (root->children[int(st[it])-97]==NULL)
+        if (root->children[pos(st[it])]==NULL)
         {
-            cout<<"yes"<<endl;
-            stnode *node = init_node(it+1,strlen(st),true);
-            root->children[int(st[it])-97]=node;
+            cout<<"written"<<endl;
+            stnode *node = init_node(it+1,strlen(st));
+            root->children[pos(st[it])]=node;
         }
         else
         {
-            cout<<"over_write"<<endl;
+            cout<<"over_written"<<it<<endl;
+            root->children[pos(st[it])]=overwrite(root->children[pos(st[it])],st,it+1);
+
         }
 
     }
-    for (int itt=0;itt<alpha_max;itt++) if(root->children[itt]!=NULL) 
-    {
-        cout<<"#"<<typeid(root->children[itt]).name()<<root->children[itt]->start<<root->children[itt]->end;}
-    cout<<endl;
+    
 
-    cout<<"--------"<<endl;
+    cout<<"-------------------------------------------------------"<<endl;
     stdisplay(root,st);
 
 }
