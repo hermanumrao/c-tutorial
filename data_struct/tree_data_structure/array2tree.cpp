f1a3d6e927070d87cb64fbe4e30ac328cec38b55
Herman Singh Umrao <63940157+hermanumrao@users.noreply.github.com>
2022-10-30 12:31:59 +0530

    Add files via upload

---
+++ data_struct/tree_data_structure/array2tree.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std ;
+
+/*
+      1
+     / \ 
+    2   3 
+   / \  /\
+  4  5 6  7
+ /\
+8  9
+        */
+
+struct treenode
+{
+    int value;
+    struct treenode *left;
+    struct treenode *right;
+
+};
+
+treenode *crt_node(int value)
+{
+    treenode *node =new treenode();
+    node->value=value;
+    node->left=NULL;
+    node->right=NULL;
+    return node;
+}
+
+
+
+void print_tabs(int i)
+{
+    while (i>0)
+    {
+        cout<<"\t";
+        i=i-1;
+    }
+}
+void print_tree(treenode *root, int level)
+{
+    if (root==NULL)
+    {
+        cout<<"-- ";
+        return;
+    }
+    cout<<"value="<<root->value<<endl;
+    print_tabs(level);
+    cout<<"left=";
+    print_tree(root->left,level+1);
+
+    print_tabs(level);
+    cout<<"right=";
+    print_tree(root->right,level+1);
+    cout<<endl;
+}
+
+void ins_tree(treenode *root,int *arr,int &i,int maxl)
+{
+    if (2*i+1>=maxl) return;
+    root->left=crt_node(arr[2*i+1]);
+    if (2*i+2>=maxl) return;
+    root->right=crt_node(arr[2*i+2]);
+    int a=2*i+1;
+    ins_tree(root->left,arr,a,maxl);
+
+    int b=2*i+2;
+    ins_tree(root->right,arr,b,maxl);
+
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    treenode *root = crt_node(arr[0]);
+    int i=0;
+    ins_tree(root,arr,i,(sizeof(arr)/sizeof(arr[0])));
+    print_tree(root,0);
+    return 0;
+}
