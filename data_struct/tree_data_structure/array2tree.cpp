e54b4827a0e70ca400c28950dd89f32af15a29a8
Herman Singh Umrao <63940157+hermanumrao@users.noreply.github.com>
2022-10-30 12:34:17 +0530

    Update array2tree.cpp

--- data_struct/tree_data_structure/array2tree.cpp
+++ data_struct/tree_data_structure/array2tree.cpp
@@ -31,7 +31,7 @@ treenode *crt_node(int value)
 }
 
 
-
+//part of printing
 void print_tabs(int i)
 {
     while (i>0)
@@ -57,8 +57,8 @@ void print_tree(treenode *root, int level)
     print_tree(root->right,level+1);
     cout<<endl;
 }
-
-void ins_tree(treenode *root,int *arr,int &i,int maxl)
+//functin to create tree out of array
+void ins_val(treenode *root,int *arr,int &i,int maxl)
 {
     if (2*i+1>=maxl) return;
     root->left=crt_node(arr[2*i+1]);
@@ -74,7 +74,7 @@ void ins_tree(treenode *root,int *arr,int &i,int maxl)
 
 int main()
 {
-    int arr[]={1,2,3,4,5,6,7,8,9};
+    int arr[]={1,2,3,4,5,6,7,8,9};//insert your array here
     treenode *root = crt_node(arr[0]);
     int i=0;
     ins_tree(root,arr,i,(sizeof(arr)/sizeof(arr[0])));
