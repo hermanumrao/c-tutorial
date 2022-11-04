82b2b20afab5af93cade2688d4ddb4e27794c840
Herman Singh Umrao <63940157+hermanumrao@users.noreply.github.com>
2022-11-04 22:49:07 +0530

    Create search_BST.cpp

---
+++ data_struct/tree_data_structure/search_BST.cpp
@@ -0,0 +1,105 @@
+//convert array to BST
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include<stdbool.h>
+using namespace std ;
+
+
+/*
+[1,2,3,4,5,6,7,8]
+
+       4
+     /  \ 
+    2    6 
+   / \   /\
+  1  3  5  7
+            \
+             8 
+        
+*/
+
+struct treenode
+{
+    int value;
+    struct treenode *left;
+    struct treenode *right;
+
+};
+treenode *crt_node(int value )
+{
+    treenode *node =new treenode();
+    node->value=value;
+    node->left=NULL;
+    node->right=NULL;
+    return node;
+}
+
+void print_tabs(int i)
+{
+    while (i>0)
+    {
+        cout<<"\t";
+        i=i-1;
+    }
+}
+void print_tree(treenode *root, int level)
+{
+    if (root==NULL)
+    {
+        cout<<"-- ";
+        return;
+    }
+    cout<<"value="<<root->value<<endl;
+    print_tabs(level);
+    cout<<"left=";
+    print_tree(root->left,level+1);
+
+    print_tabs(level);
+    cout<<"right=";
+    print_tree(root->right,level+1);
+    cout<<endl;
+}
+
+void search_tree(treenode *root, int item, bool &found)
+{
+    int val=root->value;
+    if (val==item) 
+    {
+        found=true ;
+        return;
+    }
+    if (root->left==NULL && root->right==NULL) return;
+    if (val>item) search_tree(root->left,item,found);
+    else search_tree(root->right,item,found);
+}
+
+
+treenode *ins_tree(int *arr,int start,int end)
+{
+    if (end<start) return NULL;
+    int mid=(start+end)/2;
+    treenode *root=crt_node(arr[mid]);
+    root->left=ins_tree(arr,start,mid-1);
+    root->right=ins_tree(arr,mid+1,end);
+    return root;
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8};
+    treenode *root = crt_node(arr[0]);
+    int l=sizeof(arr)/sizeof(arr[0]);
+    int i=1;
+    root=ins_tree(arr,0,l-1);
+    print_tree(root,0);
+    int item;
+    cin>>item;
+    bool found=false;
+    search_tree(root,item,found);
+    if(found==false) cout<<"item is not in array"<<endl;
+    else cout<<"found item";
+
+    return 0;
+}
+
