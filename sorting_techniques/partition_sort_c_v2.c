9576a37977c534cc713db5f307244fa6eee0e148
Herman Singh Umrao <hermanumrao@gmail.com>
2022-11-12 22:33:53 +0530

    minor changes

--- sorting_techniques/partition_sort_c_v2.c
+++ sorting_techniques/partition_sort_c_v2.c
@@ -26,15 +26,7 @@ void merge(int *L , int l,int *R,int r, int *v)
 		printf("%s\n","invalid values");
 		return;
 	}
-	// int x=(l+r)* (sizeof (int));
-	// int *a =(int*) malloc(x);
-	// memcpy(a,v,x);
-	display(L,l);
-	printf("%s\n","--L");
-	display(R,r);
-	printf("%s\n","--R");
-	printf("%s\t","initial");
-	display(v,l+r);
+	
 
 	while(i<l && j<r)
 	{
@@ -55,8 +47,6 @@ void merge(int *L , int l,int *R,int r, int *v)
 		}
 		
 	}
-	//printf("%s\t","merge 1");
-	//display(v,k);
 	while (i<l || j<r)
 	{
 		if (l==i)
@@ -72,10 +62,7 @@ void merge(int *L , int l,int *R,int r, int *v)
 			++k;
 		}
 	}
-	printf("%s\n","merge 2");
-	display(v,k);
-	//memcpy(v,a,x);
-	//free(a);
+	
 }
 
 
@@ -87,27 +74,17 @@ void partition(int *v,int s)
 	int cl[s];
 	int cr[s-s/2];
 	
-	// display(cl,s/2);
-	// display(cr,s-s/2);
+	
 	if (s>1)
 	{
 		partition(L,s/2);
 		partition(R,(s-s/2));
 		memcpy(cl,L,(s/2* sizeof(int)));
 		memcpy(cr,R, (s-s/2)* sizeof(int));
-		// printf("%s","L ");
-		// display(L,s/2);
-		// display(cl,s/2);
-		// printf("%s","R ");
-		// display(R,(s-s/2));
-		// display(cr,s-s/2);
+	
 		merge(cl,s/2,cr,(s-s/2),v);
 	}
 
-	// printf("%s\t","   " );
-	// printf("%d\n",*L );
-	// printf("%s\t","   " );
-	// printf("%d\n",*R );
 	
 	
 }
@@ -116,20 +93,9 @@ void partition(int *v,int s)
 int main ()
 {
 	int l=0;
-	int v[]={2,3,1};
+	int v[]={4,7,6,5,3,8,2,9,1};
 	int m=sizeof(v)/sizeof(v[0]);
-	//display(v,m);
-	// int a=m/2;
-	// int b=m-m/2;
-	// printf("%d\n",m);
-	// printf("%d\n",a);
-	// printf("%d\n",b);
-
 	partition(v,m);
-	// int L[]={4};
-	// int R[]={2};
-	// merge(L,1,R,1,v);
-	printf("%s\n","---" );
-	//display(v,m);
+	display(v,m);
 	return 0;
 }
