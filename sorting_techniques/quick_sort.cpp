cee2c45680c3436870590cf8f1e28e353337ff00
Herman Singh Umrao <hermanumrao@gmail.com>
2022-11-13 19:22:38 +0530

    sort sucessful

--- sorting_techniques/quick_sort.cpp
+++ sorting_techniques/quick_sort.cpp
@@ -3,7 +3,6 @@
 #include <stdio.h>//to compile in c
 #include <stdlib.h>// to compile in c
 #include<iostream>
-#include <string>// used for concatenation of the arrays
 using namespace std ;
 
 void disp_arr(int *arr, int l)
@@ -16,57 +15,63 @@ void disp_arr(int *arr, int l)
     cout<<endl;
 }
 
-void qsort(int *arr,int st, int end, int *narr)
+void swap(int *x,int *y)
+{
+	int tmp=*x;
+	*x=*y;
+	*y=tmp;
+}
+
+void qsort(int *arr,int st, int end)
 {
 	if ((end-st)<1 ) return ;
 	int mid= (st+end)/2;
-	int L[mid-st];
-	int R[end-mid];
 	int l=0;
-	int r=0;
-	for (int i=st;i<(end+1);i++)
+	int r=mid;
+	while (l< mid && r<end)
 	{
-		if (arr[i]<arr[mid]) 
+		while(l<mid) 
 		{
-			L[l]=arr[i];
-			l++;
-		}
-		else if (arr[i]>arr[mid])
+			if (arr[l]>arr[mid]) break;
+			else l++;
+ 		}
+ 		while(r<(end+1))
+ 		{
+ 			if (arr[r]<arr[mid]) break;
+			else r++;
+ 		}
+		if (r<(end+1) && l<mid) swap(arr[l],arr[r]);
+		else
 		{
-			R[r]=arr[i];
-			r++;
+			if (r<(end+1))
+			{
+				if((r-mid)>1)
+				{
+					swap(arr[mid],arr[mid+1]);
+					swap(arr[mid],arr[r]);
+					mid=mid+1;
+					l++;
+
+				}
+				else swap(arr[r],arr[mid]);
+			}
+			if (l<mid)
+			{
+				swap(arr[l],arr[mid]);
+				mid=l;
+			}
 		}
 	}
-	qsort(L,st,mid,narr);
-	if (l>1) for (int it3=0;it3<l;it3++) L[it3]=narr[it3];
-	qsort(R,0,r-1,narr);
-	if (r>1) for (int it4=0;it4<r;it4++) R[it4]=narr[it4];
-
-	for (int it1=0; it1<(end-st+1);it1++)
-	{
-		for (int it2=0; it2<(l);it2++)
-		{
-			narr[it1]=L[it2];
-			it1++;
-		}
-		narr[it1]=arr[mid];
-		it1++;
-		for (int it2=0; it2<(r);it2++)
-		{
-			narr[it1]=R[it2];
-			it1++;
-		}
-	}
-	return ;
-
+	qsort(arr,st,mid-1);
+	qsort(arr,mid+1,end);
 }
  
 int main()
 {
-	int arr[]={4,7,6,5,3,1,2};
+	int arr[]={4,9,8,6,53,1,2};
 	int l=sizeof(arr)/sizeof(arr[0]);
-	int narr[l];
-	qsort(arr,0,l-1,narr);
-	disp_arr(narr,l);
+	disp_arr(arr,l);
+	qsort(arr,0,l-1);
+	disp_arr(arr,l);
 	return 0;
 }
