05afa9924c8cf8b58295787e7754528dd88a5fc0
hermanumrao <hermanumrao@gmail.com>
2023-04-21 20:12:52 +0530

    my first matrix program

---
+++ data_struct/matrix/matrix_basic.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int xrow=4;
+const int xcol=5;
+
+int main()
+{
+	int it=1;
+	int matrix1[xrow][xcol];
+	for(int i=0;i<xrow;i++)
+	{
+		for(int j=0;j<xcol;j++)
+		{
+			matrix1[i][j]=it;
+			it++;
+		}
+	}
+	for(int i=0;i<xrow;i++)
+        {       
+                for(int j=0;j<xcol;j++)
+                {       
+                        cout<<matrix1[i][j]<<"\t";
+                }       
+		cout<<endl;
+        }		
+}
+
