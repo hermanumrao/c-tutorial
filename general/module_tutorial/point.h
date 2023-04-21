d635f9f80191246ca5abcea7945915da61e52e55
hermanumrao <hermanumrao@gmail.com>
2023-04-21 20:11:23 +0530

    added new comments

--- general/module_tutorial/point.h
+++ general/module_tutorial/point.h
@@ -1,7 +1,11 @@
 #ifndef POINT_H 
 #define POINT_H// this helps from stopping the program from importing this module multiple times
 // even if #include is used multiple times
+/*Once the header is included, it checks if a unique value (in this case HEADERFILE_H) is defined. Then if it's not defined, it defines it and continues to the rest of the page.
 
+When the code is included again, the first ifndef fails, resulting in a blank file.
+
+That prevents double declaration of any identifiers such as types, enums and static variables.*/
 typedef struct point
 {
         double x;
