142c5567498f27a9a061beb44cb168580e291269
hermanumrao <hermanumrao@gmail.com>
2023-08-07 20:29:22 +0530

    done with lots of differrent opperator overloads

--- general/Complex_No_overload_tutorial.cpp
+++ general/Complex_No_overload_tutorial.cpp
@@ -1,13 +1,14 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
 
 using namespace std;
 
 class Complex
 {
 	public:
-		float real;
-		float imag;
-		Complex(float r=0, float i=0) : real(r),imag(i) {}
+		int real;
+		int imag;
+		Complex(int r=0, int i=0) : real(r),imag(i) {}
 
 		//operator overloading + - *
 		Complex operator + (Complex cmp)
@@ -28,31 +29,51 @@ class Complex
 			temp.imag =real*cmp.imag+imag*cmp.real;
 			return temp;
 		}
-		
+		Complex operator ++()//overloading the preincrement operator
+		{
+			real=real+1;
+			return *this;
+		}
+		Complex operator ++( int )
+		{
+			Complex temp =*this;
+			real+=1;
+			return temp;
+		}
+		operator float()
+		{
+			return sqrt(real*real+imag*imag);
+		}
 
-		//overloading output operator <<
 	private:
-		friend ostream & operator << (ostream & ostr, const Complex &c1)
-		//overloading ostream operator to print in a better way 
-		{	return ostr << c1.real<<"+"<<c1.imag<<"i";
-		}
-		friend istream & operator >> (istream & istr, const Complex &c1)
-		//overloading ostream operator to print in a better way 
-		{	cout<<"real part:";
-			istr>>c1.real;
-			cout<<"\nimaginary part:";
-			istr>>c1.imag;
-			return istr;
-		}
-		friend int operator == (const Complex &c1 , const Complex &c2)		// for all relational operators use this syntax
-		{	
-			return (c1.real==c2.real && c1.imag==c2.imag);
-		}
+			//overloading output operator <<
+		friend ostream & operator << (ostream &, const Complex &);
+		friend istream & operator >> (istream & , const Complex &);
+		friend bool operator == (const Complex & , const Complex &);		// for all relational operators use this syntax
 };
+
+istream & operator >> (istream & istr, const Complex &c1)
+		//overloading istream operator to take input in in a better way 
+{	cout << "real part:";
+	istr >> c1.real;
+	cout << "\nimaginary part:";
+	istr >> c1.imag;
+	return istr;
+} 
+
+bool operator == (const Complex &c1 , const Complex &c2)		// for all relational operators use this syntax
+{	
+	return (c1.real==c2.real && c1.imag==c2.imag);
+}
+
+ostream & operator << (ostream & ostr, const Complex &c1)
+	//overloading ostream operator to print in a better way 
+{	return ostr << c1.real<<"+"<<c1.imag<<"i";
+}
 
 int main()
 {
-	float real,imag;
+	int real,imag;
 	cout << "enter value for real and imaginary parts for c1"<<endl;
 	cin >> real;
 	cin >> imag;
@@ -63,5 +84,10 @@ int main()
 	Complex c2(real,imag),res;
 	res = c1+c2;
 	cout<<res<<endl;
+	cout<<++res<<endl;
+	cout<<res<<endl;
+	cout<<res++<<endl;
+	cout<<res<<endl;
 	cout<<(c1==c2);
+	cout<<float(res);
 }
