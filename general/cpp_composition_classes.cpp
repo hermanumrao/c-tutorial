532746c61d09ed20cc28f6f8a8cf37de9e7210a3
hermanumrao <hermanumrao@gmail.com>
2024-01-07 22:58:15 +0530

    added new stuff

---
+++ general/cpp_composition_classes.cpp
@@ -0,0 +1,11 @@
+//  Composition of classes refers to use of one or more classes within the definition of another class
+
+#include<bits/stdc++.h>
+
+class Date 
+{
+	istream & operator>> (istream & istr, Date &d)
+	{
+		
+	}
+}
